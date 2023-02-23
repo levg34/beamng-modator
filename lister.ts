a085cf89a36a6875c535e99cd7908064dbfe89ba
levg34 <levg34@users.noreply.github.com>
2023-02-23 18:15:23 +0000

    return the config files list

--- lister.ts
+++ lister.ts
@@ -2,6 +2,12 @@ import fs from 'fs/promises'
 
 export const MODS_BASEPATH = './data/'
 
+export interface ConfigData {
+    infoFile: string,
+    name: string
+    pcFile?: string,
+}
+
 export async function listMods(): Promise<string[]> {
     const mods = await fs.readdir(MODS_BASEPATH)
     return mods.filter(modName => modName !== '.gitkeep')
@@ -12,8 +18,26 @@ export async function listVehicles(modName: string): Promise<string[]> {
     return vehicles.filter(vehicle => vehicle !== 'common')
 }
 
-export async function listConfigs(modName:string, vehicle: string): Promise<string[]> {
+const infoFileToConfigName = (infoFile: string): string => infoFile.replace('info_', '').replace('.json', '')
+
+export async function listConfigs(modName:string, vehicle: string, configFiles?: boolean): Promise<string[] | ConfigData[]> {
     const vehicleContent = await fs.readdir(MODS_BASEPATH+modName+'/vehicles/'+vehicle)
     const infoFiles = vehicleContent.filter(file => file.startsWith('info_') && file.endsWith('.json'))
-    return infoFiles.map(infoFile => infoFile.replace('info_','').replace('.json',''))
+    const configs = infoFiles.map(infoFileToConfigName)
+    if (configFiles) {
+        const cf: ConfigData[] = infoFiles.map(file => {
+            const confName = infoFileToConfigName(file)
+            const res: ConfigData = {
+                name: confName,
+                infoFile: file
+            }
+            const pcFileName = confName + '.pc'
+            if (vehicleContent.includes(pcFileName)) {
+                res.pcFile = pcFileName
+            }
+            return res
+        })
+        return cf
+    }
+    return configs
 }
--- tests/lister/lister.test.ts
+++ tests/lister/lister.test.ts
@@ -14,10 +14,21 @@ describe('Test mod lister', () => {
     })
 
     it('should list correctly the vehicles', async () => {
-        expect(await listVehicles(TESTED_MOD)).toEqual(['etk800', 'fullsize', 'hopper', 'scintilla', 'vivace'])
+        expect(await listVehicles(TESTED_MOD)).toEqual(['etk800', 'fullsize', 'hopper', 'scintilla', 'test_vehicle', 'vivace'])
     })
 
     it('should list correctly the configurations', async () => {
         expect(await listConfigs(TESTED_MOD, TESTED_VEHICLE)).toEqual([EXPECTED_CONFIG.name])
+    })
+
+    it('should list correctly the configuration files', async () => {
+        expect(await listConfigs(TESTED_MOD, TESTED_VEHICLE, true)).toEqual([EXPECTED_CONFIG])
+    })
+
+    it('should list correctly the configuration files with no PC file', async () => {
+        expect(await listConfigs(TESTED_MOD, 'test_vehicle', true)).toEqual([{
+            name: 'testv',
+            infoFile: 'info_testv.json'
+        }])
     })
 })
