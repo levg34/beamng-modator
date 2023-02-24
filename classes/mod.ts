2585c65df9e45aeda68498e118ff060147e5dc58
levg34 <levg34@users.noreply.github.com>
2023-02-24 15:12:50 +0000

    implemet mod and vehicle

--- classes/mod.ts
+++ classes/mod.ts
@@ -1,3 +1,20 @@
+import { listVehicles } from "../actioner/lister"
+import { Vehicle } from "./vehicle"
+
 export class Mod {
-    //
+    modName: string
+
+    constructor(modName: string) {
+        this.modName = modName
+    }
+
+    async listVehicles(): Promise<string[]> {
+        const vehicleList = await listVehicles(this.modName)
+        return vehicleList
+    }
+
+    async getVehicles(): Promise<Vehicle[]> {
+        const vehicleList = await this.listVehicles()
+        return vehicleList.map(v => new Vehicle(this.modName, v))
+    }
 }
--- classes/vehicle.ts
+++ classes/vehicle.ts
@@ -1,3 +1,22 @@
+import { listConfigs } from "../actioner/lister"
+import { ConfigLoader } from "./config"
+
 export class Vehicle {
-    //
+    modName: string
+    vehicle: string
+
+    constructor(modName: string, vehicle: string) {
+        this.modName = modName
+        this.vehicle = vehicle
+    }
+
+    async listConfigs(): Promise<string[]> {
+        const configs = await listConfigs(this.modName,this.vehicle) as string[]
+        return configs
+    }
+
+    async getConfigLoaders(): Promise<ConfigLoader[]> {
+        const configs = await this.listConfigs()
+        return configs.map(c => new ConfigLoader(this.modName, this.vehicle, c))
+    }
 }
