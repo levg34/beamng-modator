5956d0d339ae43fbd70dd0b191a2b2665336104c
levg34 <levg34@users.noreply.github.com>
2023-07-03 18:22:09 +0000

    main as script

--- main.ts
+++ main.ts
@@ -1,61 +1,43 @@
-import { Mod } from "./classes/mod";
+import { Mod } from './classes/mod'
+import yargs from 'yargs'
+import fs from 'fs/promises'
+import { ProgramCreator } from './classes/program'
 
-// const mod = new Mod('GendarmerieNationaleSkinPack')
-const mod = new Mod('unmarkedbeaconkemro')
+const options = yargs
+    .usage('Usage: --modName <modName> --programFile <programFile> --action <action>')
+    .option('m', { alias: 'modName', describe: 'The name of the mod to process', type: 'string', demandOption: true })
+    .option('p', { alias: 'programFile', describe: 'The JSON file with the program instructions', type: 'string', demandOption: true })
+    .option('a', { alias: 'action', describe: 'The action to perform: apply or create', type: 'string', choices: ['apply', 'create'], demandOption: true })
+    .argv
 
 const main = async () => {
-    // const programCreator = new ProgramCreator(mod)
-    
-    // const program = await programCreator.createProgram();
-    // const json = JSON.stringify(program)
-    // fs.writeFile('new-program.json', json)
+    try {
+        const { m: modName, p: programFile, a: action } = await options
 
-    // programCreator.applyProgram({
-    //     "modName": "GendarmerieNationaleSkinPack",
-    //     "vehicles": [
-    //         {
-    //             "vehicle": "etk800",
-    //             "configs": [
-    //                 {
-    //                     "configName": "gendarmerie_new",
-    //                     "newConfig": "Gendarmerie"
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             "vehicle": "hopper",
-    //             "configs": [
-    //                 {
-    //                     "configName": "Gendarmerie",
-    //                     "newConfig": "Gendarmerie"
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             "vehicle": "scintilla",
-    //             "configs": [
-    //                 {
-    //                     "configName": "ERI",
-    //                     "newConfig": "Gendarmerie"
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             "vehicle": "vivace",
-    //             "configs": [
-    //                 {
-    //                     "configName": "vivace_skin_ERI",
-    //                     "newConfig": "Gendarmerie"
-    //                 }
-    //             ]
-    //         }
-    //     ]
-    // })
+        const mod = new Mod(modName)
+        const programCreator = new ProgramCreator(mod)
 
-    // -------------------------------------------------------------------------------------
-
-    // await mod.unzip()
-    await mod.zip()
+        if (action === 'apply') {
+            console.info(`Applying program from ${programFile} to ${modName}`)
+            const program = JSON.parse(await fs.readFile(programFile, 'utf8'))
+            programCreator.applyProgram(program)
+            console.info(`Zipping ${modName}`)
+            await mod.zip()
+            console.info(`Done!`)
+        } else if (action === 'create') {
+            console.info(`Unzipping ${modName}`)
+            await mod.unzip()
+            console.info(`Creating program from ${modName}`)
+            const program = await programCreator.createProgram()
+            const json = JSON.stringify(program)
+            console.info(`Writing program to ${programFile}`)
+            await fs.writeFile(programFile, json)
+            console.info(`Done!`)
+        }
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
 }
 
 main()
