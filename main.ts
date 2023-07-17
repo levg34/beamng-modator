325b902492cc8b45ec043cc5c961e0d32ca6f8a1
levg34 <levg34@users.noreply.github.com>
2023-07-17 19:01:38 +0000

    default program file to program/modName.json

--- main.ts
+++ main.ts
@@ -3,27 +3,33 @@ import yargs from 'yargs'
 import fs from 'fs/promises'
 import { ProgramCreator } from './classes/program'
 
+const PROGRAM_PATH = 'program/'
+
 const options = yargs
     .usage('Usage: --modName <modName> --programFile <programFile> --action <action> --list')
     .option('m', { alias: 'modName', describe: 'The name of the mod to process', type: 'string' })
     .option('p', { alias: 'programFile', describe: 'The JSON file with the program instructions', type: 'string' })
     .option('a', { alias: 'action', describe: 'The action to perform: apply or create', type: 'string', choices: ['apply', 'create'] })
-    .option('l', { alias: 'list', describe: 'List the available mods', type: 'boolean'})
-    .implies('m', ['p', 'a'])
+    .option('l', { alias: 'list', describe: 'List the available mods', type: 'boolean' })
+    .implies('m', ['a'])
     .argv
 
 const main = async () => {
     try {
-        const { m: modName, p: programFile, a: action, l: list } = await options
-
+        const { m: modName, p, a: action, l: list } = await options
+        
         if (list) {
             console.info(`List of available mods:`)
             const mods = await Mod.listMods()
             mods.forEach(mod => console.info(mod))
             return
         }
+        
+        if (!(modName && action)) throw new Error('ERROR: modName and action required when --list is not used.')
+        
+        let programFile = p
 
-        if (!(modName && programFile && action)) throw new Error('ERROR: modName, programFile and action required when --list is not used.')
+        if (!programFile) programFile = PROGRAM_PATH + modName + '.json'
 
         const mod = new Mod(modName)
         const programCreator = new ProgramCreator(mod)
@@ -40,7 +46,7 @@ const main = async () => {
             await mod.unzip()
             console.info(`Creating program from ${modName}`)
             const program = await programCreator.createProgram()
-            const json = JSON.stringify(program)
+            const json = JSON.stringify(program, null, 4)
             console.info(`Writing program to ${programFile}`)
             await fs.writeFile(programFile, json)
             console.info(`Done!`)
